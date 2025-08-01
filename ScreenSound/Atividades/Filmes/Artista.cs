7e7db13f04f87b7857168057dc64d72c4db9cc2f
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-07-31 21:32:50 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Atividades/Filmes/Artista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Atividades.Filmes
+{
+	public class Artista
+	{
+		public Artista() { }
+		public Artista(string nome, int idade)
+		{
+			Nome = nome;
+			Idade = idade;
+		}
+		public string Nome { get; set; }
+		public int Idade { get; set; }
+		public List<Filme> Filmes { get; set; } = new List<Filme>();
+	}
+}
---
+++ ScreenSound/Atividades/Filmes/Filme.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Atividades.Filmes
+{
+	public class Filme
+	{
+		public Filme() { }
+		public Filme(string titulo, int duracao)
+		{
+			Titulo = titulo;
+			Duracao = duracao;
+		}
+		public string Titulo { get; set; }
+		public int Duracao { get; set; }
+
+		public List<Artista> Artistas { get;} = new List<Artista>();
+
+		public void AdicionarArtista(Artista artista)
+		{
+			if (artista == null)
+			{
+				throw new ArgumentNullException(nameof(artista), "Artista não pode ser nulo.");
+			}
+			if (Artistas.Contains(artista))
+			{
+				throw new InvalidOperationException("Artista já adicionado ao filme.");
+			}
+			Artistas.Add(artista);
+			artista.Filmes.Add(this);
+		}
+	}
+}

03987883b40f0906db3f52bc8177ba42c698ba3a
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2026-01-20 20:46:30 -0300

    Aula 03 atv -- 05

---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+internal class MenuSair : Menu
+{
+	public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
+	{
+		Console.WriteLine("Tchau tchau :)");
+	}
+
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ScreenSound.Atividades.Filmes;
+using ScreenSound.Menus;
 using ScreenSound.Modelos;
 using VideoFlix.PlataformaA;
 
@@ -11,6 +12,15 @@ Banda beatles = new ("The Beatles");
 Dictionary<string, Banda> bandasRegistradas = new();
 bandasRegistradas.Add(ira.Nome, ira);
 bandasRegistradas.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarBanda());
+opcoes.Add(2, new MenuRegistrarAlbum());
+opcoes.Add(3, new MenuMostrarBandasRegistradas());
+opcoes.Add(4, new MenuAvaliarBanda());
+opcoes.Add(5, new MenuExibirDetalhes());
+opcoes.Add(-1, new MenuSair());
+
 
 void ExibirLogo()
 {
@@ -40,181 +50,16 @@ void ExibirOpcoesDoMenu()
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
-    switch (opcaoEscolhidaNumerica)
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
     {
-        case 1:
-            RegistrarBanda();
-            break;
-        case 2:
-            RegistrarAlbum();
-            break;
-        case 3:
-            MostrarBandasRegistradas();
-            break;
-        case 4:
-            AvaliarUmaBanda();
-            break;
-        case 5:
-            ExibirDetalhes();
-            break;
-        case -1:
-            Console.WriteLine("Tchau tchau :)");
-            break;
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
-    }
-}
-
-void RegistrarAlbum()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Registro de álbuns");
-    Console.Write("Digite a banda cujo álbum deseja registrar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-		Console.Write("Agora digite o título do álbum: ");
-		string tituloAlbum = Console.ReadLine()!;
-		Banda banda = bandasRegistradas[nomeDaBanda];
-		banda.AdicionarAlbum(new Album(tituloAlbum));
-		Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
-		Thread.Sleep(4000);
-		Console.Clear();
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(bandasRegistradas);
+        if(opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
 	}
-	else
-	{
-		Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-		Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-		Console.ReadKey();
-		Console.Clear();
-    }
-
-	ExibirOpcoesDoMenu();
-}
-
-void RegistrarBanda()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Registro das bandas");
-    Console.Write("Digite o nome da banda que deseja registrar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    Banda banda = new Banda(nomeDaBanda);
-	bandasRegistradas.Add(nomeDaBanda, banda);
-    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-    Thread.Sleep(4000);
-    Console.Clear();
-    ExibirOpcoesDoMenu();
-}
-
-void MostrarBandasRegistradas()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Exibindo todas as bandas registradas na nossa aplicação");
-
-    foreach (string banda in bandasRegistradas.Keys)
-    {
-        Console.WriteLine($"Banda: {banda}");
-    }
-
-    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
-    Console.ReadKey();
-    Console.Clear();
-    ExibirOpcoesDoMenu();
-
-}
-
-void ExibirTituloDaOpcao(string titulo)
-{
-    int quantidadeDeLetras = titulo.Length;
-    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-    Console.WriteLine(asteriscos);
-    Console.WriteLine(titulo);
-    Console.WriteLine(asteriscos + "\n");
-}
-
-void AvaliarUmaBanda()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Avaliar banda");
-    Console.Write("Digite o nome da banda que deseja avaliar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[nomeDaBanda];
-		Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-        Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-        banda.AdicionarNota(nota);
-		Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-        Thread.Sleep(2000);
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
     else
     {
-        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-
-}
-
-void ExibirDetalhes()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Exibir detalhes da banda");
-    Console.Write("Digite o nome da banda que deseja conhecer melhor: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[nomeDaBanda];
-        Console.WriteLine($"\nA média da banda {nomeDaBanda} é {banda.Media}");
-        /*
-		* ESPAÇO RESERVADO PARA COMPLETAR A FUNÇÃO
-        */
-        Console.WriteLine("Digite uma tecla para votar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-
-    }
-    else
-    {
-        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
+        Console.WriteLine("Opção inválida");
     }
 }
 
-//ExibirOpcoesDoMenu();
-
-//VideoFlix.PlataformaA.Video video = new VideoFlix.PlataformaA.Video();
-//VideoFlix.PlataformaB.Video video2 = new VideoFlix.PlataformaB.Video();
-
-# region TesteFilmeArtista
-Artista artista1 = new Artista("Tom Holand", 30);
-Filme filme1 = new Filme("Homem-Aranha: Sem Volta Para Casa", 148);
-filme1.AdicionarArtista(artista1);
-
-foreach (Artista artista in filme1.Artistas)
-{
-    Console.WriteLine($"Filme: {filme1.Titulo}, Artista: {artista.Nome}, Idade: {artista.Idade}");
-}
-
-foreach(Filme filme in artista1.Filmes)
-{
-    Console.WriteLine($"Artista: {artista1.Nome}, Filme: {filme.Titulo}, Duração: {filme.Duracao} minutos");
-}
-
-#endregion
-# region TesteIncrementadorObjetos
-/*Album a1 = new Album("Barões da Pisadinha Ao Vivo");
-Album a2 = new Album("Barões da Pisadinha feat.Anitta");
-Album a3 = new Album("Barões da Pisadinha no Free Jazz Festival)");
-
-Console.WriteLine($"Total de objetos criados: { Album.ContadorDeObjetos}");*/
-#endregion
+ExibirOpcoesDoMenu();
